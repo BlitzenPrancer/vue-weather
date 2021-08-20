04b270e6663ffd67194da1d4d7cb9388205a0717
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-08-20 12:08:14 +0530

    added search box and background image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    Hola Como Estas!
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search Here ...."/>
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Puducherry, India</div>
+          <div class="date">Sunday 29 August 2021</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°</div>
+          <div class="weather">Rain</div> 
+        </div>
+      </div>
+    </main>    
   </div>
 </template>
 
@@ -9,12 +25,70 @@ export default {
   name: 'app',
   data() {
     return {
-      api_key: "f6a2c317f97367bfe8fa3e5fc517e80d"
+      api_key: "c55243ad9f7056414def1a35285485e6"
     }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box {
+  width: 100;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display:block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location{
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+}
 </style>
